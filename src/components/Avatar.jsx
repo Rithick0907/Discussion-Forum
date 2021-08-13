c4617f9844cefde41e57bf6e905419658341f50f
Rithick <rithick0907@gmail.com>
2021-08-13 12:24:03 +0530

    Avatar styled component is replaced with avatar scss

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,7 +1,12 @@
 import { MdAccountCircle } from "react-icons/md";
-import { Image } from "../styles/Avatar.styles";
 
 const Avatar = ({ size, src }) =>
-  src ? <Image src={src} size={size} /> : <MdAccountCircle size={size} />;
+  src ? (
+    <div className="avatar">
+      <img className="avatar__img" src={src} alt="user avatar" />
+    </div>
+  ) : (
+    <MdAccountCircle size={size} />
+  );
 
 export default Avatar;
--- src/styles/Avatar.styles.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Image = styled.img`
-  height: ${({ size }) => size};
-  width: ${({ size }) => size};
-  border-radius: 100%;
-`;
