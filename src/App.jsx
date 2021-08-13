caeb5bc196ba3293c648d665396198a5c66e58f7
Rithick <rithick0907@gmail.com>
2021-08-13 12:15:32 +0530

    onAuthStateChanged Listener added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { login, logout, userSelector } from "./store/auth";
+import { useDispatch, useSelector } from "react-redux";
+
 import Routes from "./Routes";
-import { useSelector } from "react-redux";
-import { userSelector } from "./store/auth";
+import { auth } from "./service/firebase.utils";
+import { useEffect } from "react";
 
 const App = () => {
   const user = useSelector(userSelector);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            email: authUser.email,
+            name: authUser.displayName,
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+          })
+        );
+      } else dispatch(logout());
+    });
+  }, [dispatch]);
+
   return <Routes user={user} />;
 };
 
